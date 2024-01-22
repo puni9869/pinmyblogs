85d56f219c8ccd8b20068977e8d9fe2c592065b3
puni9869 <punitinani1@hotmail.com>
2024-01-22 16:14:45 +0530

    chore: re-organising code

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -2,37 +2,37 @@ package database
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
+
 	"github.com/puni9869/pinmyblogs/models"
 	"github.com/puni9869/pinmyblogs/pkg/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"log"
-	"os"
-	"time"
 )
 
-// dbObj instance of database
+// dbObj is the instance of the database
 var dbObj *gorm.DB
 
-// NewLogger is the logger configuration for sql
+// NewLogger returns the SQL logger configuration
 func NewLogger() logger.Interface {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold:             time.Second, // Slow SQL threshold
+			SlowThreshold:             time.Second, // Threshold for slow SQL queries
 			LogLevel:                  logger.Info, // Log level
-			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
-			ParameterizedQueries:      true,        // Don't include params in the SQL log
-			Colorful:                  false,       // Disable color
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound errors in the logger
+			ParameterizedQueries:      true,        // Exclude parameters from the SQL log
+			Colorful:                  false,       // Disable color in logs
 		},
 	)
 	return newLogger
 }
 
+// NewConnection creates a new database connection
 func NewConnection(cfg *config.Database) (*gorm.DB, error) {
-	//follow the below connectionString
-	// "type://username:password@host:port/dbName"
 	dsn := fmt.Sprintf("%s://%s:%s@%s:%s/%s",
 		cfg.Type,
 		cfg.Username,
@@ -41,28 +41,33 @@ func NewConnection(cfg *config.Database) (*gorm.DB, error) {
 		cfg.Port,
 		cfg.DatabaseName,
 	)
+
 	var dbLogger logger.Interface
 
 	if cfg.LogSql {
 		dbLogger = NewLogger()
 	}
+
 	ormConfig := gorm.Config{
 		Logger: dbLogger,
 	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &ormConfig)
 	if err != nil {
 		return nil, err
 	}
+
 	dbObj = db
 	return db, nil
 }
 
-// RegisterModels configures the available models.
+// RegisterModels configures the available models
 func RegisterModels(db *gorm.DB) *gorm.DB {
 	_ = db.AutoMigrate(&models.User{})
 	return db
 }
 
+// Db returns the global database instance
 func Db() *gorm.DB {
 	return dbObj
 }
