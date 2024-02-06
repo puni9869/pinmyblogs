6dc5c9cb8f576b5a064546bef43a0c9b0ee84f8a
puni9869 <punitinani1@hotmail.com>
2024-02-06 14:22:46 +0530

    chore: passwd regex implementation.

--- pkg/formbinding/binding.go
+++ pkg/formbinding/binding.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"regexp"
+	"sync"
 )
 
-type FieldErrors map[string]any
+var (
+	passwordRegex *regexp.Regexp
+	once          sync.Once
+)
 
-var fieldErrors = map[string]string{
+type Field interface {
+	Error(phrase string) string
+	ValidatePassword(password string) bool
+}
+
+type FieldErrors map[string]string
+
+var errs = map[string]string{
 	"required":           ` cannot be empty.`,
 	"alpha_dash":         ` should contain only alphanumeric, dash ('-') and underscore ('_') characters.`,
 	"alpha_dash_dot":     ` should contain only alphanumeric, dash ('-'), underscore ('_') and dot ('.') characters.`,
@@ -23,11 +35,20 @@ var fieldErrors = map[string]string{
 	"unknown":            `Unknown error:`,
 }
 
-func (e FieldErrors) Error(phrase string) string {
-	if e, ok := fieldErrors[phrase]; ok {
+func (e *FieldErrors) Error(phrase string) string {
+	if e, ok := errs[phrase]; ok {
 		return e
 	}
 	return phrase
+}
+
+func (e *FieldErrors) ValidatePassword(password string) bool {
+	once.Do(func() {
+		fmt.Println("Hello here")
+		passwordRegex, _ = regexp.Compile(`^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$`)
+	})
+	fmt.Println("Hello after")
+	return passwordRegex.Match([]byte(password))
 }
 
 // Errorf format the error in the gin.H which is a context.
@@ -36,9 +57,8 @@ func Errorf(data gin.H, errs validator.ValidationErrors) map[string]any {
 		return nil
 	}
 	data["HasError"] = true
-	var f FieldErrors
+	var f Field = new(FieldErrors)
 	for _, err := range errs {
-		fmt.Println(err.Field())
 		data[err.Field()+"_Error"] = fmt.Sprintf("%s%s", err.Field(), f.Error(err.Tag()))
 		data[err.Field()+"_HasError"] = true
 	}
--- server/auth/signup.go
+++ server/auth/signup.go
@@ -7,28 +7,43 @@ import (
 	"github.com/puni9869/pinmyblogs/server/middlewares"
 	"github.com/puni9869/pinmyblogs/types/forms"
 	"net/http"
+	"sync"
 )
 
 var (
-	fieldError formbinding.FieldErrors
+	field formbinding.Field
+	once  sync.Once
 )
 
+// SignupGet is renders the signup.templ
+// c is gin.Context
 func SignupGet(c *gin.Context) {
 	c.HTML(http.StatusOK, "signup.tmpl", nil)
 }
 
+// SignupPost is a handler for handling registrations of new user
+// s is signup.SignupService container helper functions
 func SignupPost(s signup.SignupService) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		signup := middlewares.GetForm(c).(*forms.SignUpForm)
+		s := middlewares.GetForm(c).(*forms.SignUpForm)
 		ctx := middlewares.GetContext(c)
+		once.Do(func() {
+			field = new(formbinding.FieldErrors)
+		})
+		password := s.Password
+		confirmPassword := s.ConfirmPassword
 
-		password := signup.Password
-		confirmPassword := signup.ConfirmPassword
+		if field.ValidatePassword(password) {
+			ctx["Password_HasError"] = true
+			ctx["Password_Error"] = field.Error("alpha_dash_dot")
+			ctx["ConfirmPassword_HasError"] = true
+			ctx["ConfirmPassword_Error"] = field.Error("password_not_match")
+		}
 
 		if ctx["Password_HasError"] == false && len(password) != len(confirmPassword) {
 			ctx["Password_Error"] = ""
 			ctx["Password_HasError"] = true
-			ctx["ConfirmPassword_Error"] = fieldError.Error("password_not_match")
+			ctx["ConfirmPassword_Error"] = field.Error("password_not_match")
 			ctx["ConfirmPassword_HasError"] = true
 		}
 
