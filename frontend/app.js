be38110929d5dd218d40058ee460883997a233bc
puni9869 <punitinani1@hotmail.com>
2026-01-01 13:14:36 +0530

    fix: service worker fix

--- frontend/app.js
+++ frontend/app.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
-			navigator.serviceWorker.register("/service-worker.js");
+		navigator.serviceWorker.register("/statics/service-worker.js");
 	});
 }
 
