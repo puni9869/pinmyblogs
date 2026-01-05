a72f0abc00827d178fd0cdd896d5862a52aba40f
puni9869 <punitinani1@hotmail.com>
2026-01-05 08:56:18 +0530

    fix: unit test

--- models/user_test.go
+++ models/user_test.go
@@ -30,8 +30,8 @@ func TestUserModelHidePasswordWhenLogged(t *testing.T) {
 		UpdatedAt:       time.Time{},
 		AlternateEmail:  "",
 	}
-	want := "Email:  FirstName:  Id: 31313131-3131-4131-b131-313131313131"
 	got := fmt.Sprintf("%v", u)
+	want := "{31313131-3131-4131-b131-313131313131    I will not ganna tell you  false false false  0001-01-01 00:00:00 +0000 UTC 0001-01-01 00:00:00 +0000 UTC 0001-01-01 00:00:00 +0000 UTC    0001-01-01 00:00:00 +0000 UTC 0001-01-01 00:00:00 +0000 UTC}"
 	if strings.Compare(got, want) != 0 {
 		t.Errorf("Get() = %s, want %s", got, want)
 	}
